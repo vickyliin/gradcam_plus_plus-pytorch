aef2b64b7b38049349d3b762f11dd9a01f5a5d5d
vickyliin <vickyliinn@gmail.com>
2019-07-21 01:03:46 +0800

    use scm

--- setup.py
+++ setup.py
@@ -30,7 +30,8 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.0',  # Required
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
