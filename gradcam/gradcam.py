ccb5f4ce20a288b9a74fb0827a9c7e5ce088f7e4
vickyliin <vickyliinn@gmail.com>
2019-07-21 00:53:06 +0800

    fix input size

--- gradcam/gradcam.py
+++ gradcam/gradcam.py
@@ -144,7 +144,7 @@ class GradCAMpp(GradCAM):
 
         saliency_map = (weights*activations).sum(1, keepdim=True)
         saliency_map = F.relu(saliency_map)
-        saliency_map = F.upsample(saliency_map, size=(224, 224), mode='bilinear', align_corners=False)
+        saliency_map = F.upsample(saliency_map, size=(h, w), mode='bilinear', align_corners=False)
         saliency_map_min, saliency_map_max = saliency_map.min(), saliency_map.max()
         saliency_map = (saliency_map-saliency_map_min).div(saliency_map_max-saliency_map_min).data
 
